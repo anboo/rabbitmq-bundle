5999af2d8706f0a46fded77943cf9c7b73ce3f84
anboo <developer@rusdtaem.ru>
2019-04-06 18:11:44 +0300

    Fix ignored attributes

---
+++ src/Serializer/Normalizer/IgnoredAttributesNormalizer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Anboo\ApiBundle\Serializer\Normalizer;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+use Symfony\Component\PropertyInfo\PropertyTypeExtractorInterface;
+use Symfony\Component\Serializer\Mapping\ClassDiscriminatorResolverInterface;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactoryInterface;
+use Symfony\Component\Serializer\NameConverter\NameConverterInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+/**
+ * Class IgnoredAttributesNormalizer
+ */
+class IgnoredAttributesNormalizer extends ObjectNormalizer
+{
+    protected $ignoredAttributes = [
+
+    ];
+}
