a4942657de20759d47298ddb9a142c68e7f16b58
Danil Andreyev <rusdteam@gmail.com>
2021-10-20 12:43:29 +0300

    Fix parse dsn

--- src/AMQP/AMQPConnection.php
+++ src/AMQP/AMQPConnection.php
@@ -9,4 +9,49 @@ use PhpAmqpLib\Connection\AMQPLazyConnection;
  */
 class AMQPConnection extends AMQPLazyConnection
 {
+    public function __construct(
+        $host,
+        $port,
+        $user,
+        $password,
+        $vhost = '/',
+        $insist = false,
+        $login_method = 'AMQPLAIN',
+        $login_response = null,
+        $locale = 'en_US',
+        $connection_timeout = 3.0,
+        $read_write_timeout = 3.0,
+        $context = null,
+        $keepalive = false,
+        $heartbeat = 0,
+        $channel_rpc_timeout = 0.0,
+        $ssl_protocol = null
+    ) {
+        $parsedDsn = parse_url($_ENV['MESSENGER_TRANSPORT_DSN']);
+        if ($parsedDsn) {
+            $host = $parsedDsn['host'];
+            $port = $parsedDsn['port'];
+            $user = $parsedDsn['user'];
+            $password = $parsedDsn['pass'];
+        }
+
+        parent::__construct(
+            $host,
+            $port,
+            $user,
+            $password,
+            $vhost,
+            $insist,
+            $login_method,
+            $login_response,
+            $locale,
+            $connection_timeout,
+            $read_write_timeout,
+            $context,
+            $keepalive,
+            $heartbeat,
+            $channel_rpc_timeout,
+            $ssl_protocol
+        );
+    }
 }
